5792f8099aa4e896db5a68035b3ff65424580a01
Cristina <crismarort22@gmail.com>
2017-12-19 18:02:04 +0100

    Original Cristina

---
+++ Ejercicio_1_CM.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 19 17:54:45 2017
+
+@author: crist
+"""
+
+# Funciones:
+
+
+def n_elementos_pos(l):
+    acum = 0
+    for i in l:
+        if i>0 :
+            acum +=1
+        else:
+            acum += 0
+    return acum   
+
+
+
+def suma_saltando(l,i,n):
+    acum= 0
+    for i in l[i: : n]:
+        acum += i
+    return acum 
+
+
+def divisores(x):
+    res= []
+    for i in range(1,x+1):
+        if x % i== 0:
+            res.append(i)
+    return res  
+
+def covarianza(l,m):
+    medial=sum(l)/len(l)
+    mediam=sum(m)/len(m)
+    n=len(l)
+    acum=0
+    for i,j in zip(l,m):
+        acum +=((i-medial)*(j-mediam))/n
+    return acum  
