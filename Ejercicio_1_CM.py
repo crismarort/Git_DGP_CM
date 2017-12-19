2c423c95291b458e25340a215777829f1ff4805f
Cristina <crismarort22@gmail.com>
2017-12-19 18:10:13 +0100

    Cambio

--- Ejercicio_1_CM.py
+++ Ejercicio_1_CM.py
@@ -12,23 +12,23 @@ def n_elementos_pos(l):
     acum = 0
     for i in l:
         if i>0 :
-            acum +=1
+            acum +=3
         else:
-            acum += 0
+            acum += 1
     return acum   
 
 
 
 def suma_saltando(l,i,n):
     acum= 0
-    for i in l[i: : n]:
+    for i in l[i: 2 : n]:
         acum += i
     return acum 
 
 
 def divisores(x):
     res= []
-    for i in range(1,x+1):
+    for i in range(1,x//2):
         if x % i== 0:
             res.append(i)
     return res  
